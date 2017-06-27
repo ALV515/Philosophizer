c27a0271253d24e4b6de59ac2eec4158cc772520
Alex Vaccari <alexvaccari@Alexs-MacBook-Air.local>
2017-06-26 21:54:54 -0400

    Test of JSOUP selector methods

--- src/LinkCrawler.java
+++ src/LinkCrawler.java
@@ -1,5 +1,34 @@
 /**
  * Created by alexvaccari on 6/26/17.
  */
-public class LinkCrawler {
+
+/*
+Crawls wikipedia until the Philosophy page is reached.
+ */
+
+import java.io.*;
+import java.net.*;
+import org.jsoup.Jsoup;
+import org.jsoup.helper.Validate;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+public class LinkCrawler{
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader urlGet = new BufferedReader(new InputStreamReader(System.in));
+
+        String url = "https://en.wikipedia.org/wiki/Machine";
+
+        Document doc = Jsoup.connect(url).get();
+
+        Elements links = doc.select("a[href]");
+
+        for(Element link : links){
+            System.out.println(link);
+        }
+    }
+
 }
